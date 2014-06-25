75d64161e4b1f6a92cf75a81e5c844db7ee264c3
Scott Landes <scottlandes@gmail.com>
2014-06-25 13:23:16 -0700

    Removed global var

--- src/static/js/app/todo/collections/todos.js
+++ src/static/js/app/todo/collections/todos.js
@@ -4,8 +4,6 @@ define(function(require, exports, module) {
     var Todo = require('app/todo/models/todo');
     var localStorage = require('localStorage');
 
-    window.TodoModel = Todo;
-
     return Backbone.Collection.extend({
         localStorage: new Backbone.LocalStorage("todos"),
 
